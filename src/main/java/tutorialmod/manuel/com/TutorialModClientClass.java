585036a5fb81c25807db882b029cfa64af331ef9
Manuel-Cam <manuelcam.contacto@protonmail.com>
2025-02-02 21:25:41 +0100

    Current state of the project

---
+++ src/main/java/tutorialmod/manuel/com/TutorialModClientClass.java
@@ -0,0 +1,10 @@
+package tutorialmod.manuel.com;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClientClass implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        //TO-DO
+    }
+}
